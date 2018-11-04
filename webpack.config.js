9faa686719b0c2ee6f527bad80c2cdfdd3eba7a3
sumirpv <sumirpv@gmail.com>
2018-11-04 10:02:04 -0800

    config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,58 @@
+const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports ={
-    entry:'./index.js',
-    output: {
-        path: __dirname,
-        filename:'./bundle.js'
-    }, 
-    module:{
-        rules:[
-            {
-            test:'/\.js$',
-            loader:'babel-loader'
-        }
-    ]
-    }
-}
+//require('dotenv').config();
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+   mode: 'development',
+  entry: [ './index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+    })
+  ]
+};
+
+// const path = require('path');
+// const webpack = require('webpack');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports ={
+//     context: path.join(__dirname, 'src'),
+//         mode: 'development',
+//     entry:'./index.js',
+//     output: {
+//         path:path.resolve(__dirname,'dist'),
+//         filename:'./bundle.js'
+//     }, 
+//     module:{
+//         rules:[
+//             {
+//             test:'/\.js$',
+//             loader:'babel-loader'
+//         }
+//     ]
+//     },
+//     plugins: [
+//         new HtmlWebpackPlugin({
+//             template: './index.html',
+//             inject: 'body'
+//         })
+//     ]
+// }
